9cb98aab7e65b38abe395a4d0f011d7d600d8898
xrabcde <xrabcde@gmail.com>
2022-07-02 19:11:32 +0900

    feat: 기본 구분자를 사용하는 경우 기능 구현

--- src/main/kotlin/calculator/Calculator.kt
+++ src/main/kotlin/calculator/Calculator.kt
@@ -5,4 +5,12 @@ class Calculator {
         val numbers = extractNumbers(input)
         return numbers.sumOf { it.toInt() }
     }
+
+    private fun extractNumbers(input: String): List<String> {
+        return input.split(DEFAULT_DELIMITERS).map { it.trim() }
+    }
+
+    companion object {
+        private val DEFAULT_DELIMITERS = "[,:]".toRegex()
+    }
 }
