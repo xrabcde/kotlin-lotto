a3399d90c6ca9558a14e617dc55214612d9e478d
xrabcde <xrabcde@gmail.com>
2022-07-02 19:12:04 +0900

    feat: 커스텀 구분자를 사용하는 경우 기능 구현

--- src/main/kotlin/calculator/Calculator.kt
+++ src/main/kotlin/calculator/Calculator.kt
@@ -7,10 +7,26 @@ class Calculator {
     }
 
     private fun extractNumbers(input: String): List<String> {
+        return if (input.startsWith(CUSTOM_DELIMITER_START_SIGN)) {
+            extractCustomCase(input)
+        } else {
+            extractDefaultCase(input)
+        }
+    }
+
+    private fun extractCustomCase(input: String): List<String> {
+        val customDelimiter = input.substringAfter(CUSTOM_DELIMITER_START_SIGN).substringBefore(CUSTOM_DELIMITER_END_SIGN)
+        val numbers = input.substringAfter(CUSTOM_DELIMITER_END_SIGN)
+        return numbers.split(customDelimiter).map { it.trim() }
+    }
+
+    private fun extractDefaultCase(input: String): List<String> {
         return input.split(DEFAULT_DELIMITERS).map { it.trim() }
     }
 
     companion object {
+        private const val CUSTOM_DELIMITER_START_SIGN = "//"
+        private const val CUSTOM_DELIMITER_END_SIGN = "\\n"
         private val DEFAULT_DELIMITERS = "[,:]".toRegex()
     }
 }
