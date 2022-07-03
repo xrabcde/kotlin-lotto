ade4cc70e6ee0820c35bbe95fd74d09bdecc50ac
xrabcde <xrabcde@gmail.com>
2022-07-03 17:25:08 +0900

    refactor: 문자열 분리하는 로직 StringSeparator로 분리

--- src/main/kotlin/calculator/Calculator.kt
+++ src/main/kotlin/calculator/Calculator.kt
@@ -1,27 +1,12 @@
 package calculator
 
 class Calculator {
+    private val separator = StringSeparator()
+
     fun calculate(input: String): Int {
-        val extractedNumbers = extractNumbers(input)
+        val extractedNumbers = separator.separate(input)
         val numbers = extractedNumbers.map { convertNumber(it) }
         return doAddition(numbers)
-    }
-
-    private fun extractNumbers(input: String): List<String> {
-        return when (input.startsWith(CUSTOM_DELIMITER_START_SIGN)) {
-            true -> extractCustomCase(input)
-            false -> extractDefaultCase(input)
-        }
-    }
-
-    private fun extractCustomCase(input: String): List<String> {
-        val customDelimiter = input.substringAfter(CUSTOM_DELIMITER_START_SIGN).substringBefore(CUSTOM_DELIMITER_END_SIGN)
-        val numbers = input.substringAfter(CUSTOM_DELIMITER_END_SIGN)
-        return numbers.split(customDelimiter).map { it.trim() }
-    }
-
-    private fun extractDefaultCase(input: String): List<String> {
-        return input.split(DEFAULT_DELIMITERS).map { it.trim() }
     }
 
     private fun convertNumber(input: String): Int {
@@ -39,11 +24,5 @@ class Calculator {
         if (inputs.any { it < 0 }) {
             throw RuntimeException("음수는 입력하실 수 없습니다.")
         }
-    }
-
-    companion object {
-        private const val CUSTOM_DELIMITER_START_SIGN = "//"
-        private const val CUSTOM_DELIMITER_END_SIGN = "\\n"
-        private val DEFAULT_DELIMITERS = "[,:]".toRegex()
     }
 }
