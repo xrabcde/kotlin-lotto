b3957c70336078a91ccf51a6242f799125af655e
xrabcde <xrabcde@gmail.com>
2022-07-05 00:37:47 +0900

    feat: 구매금액이 양수가 아닌 경우 예외처리 추가

--- src/main/kotlin/lotto/controller/LottoMachine.kt
+++ src/main/kotlin/lotto/controller/LottoMachine.kt
@@ -11,10 +11,21 @@ class LottoMachine {
     }
 
     fun validateMoney(input: String): Int {
+        return validatePositive(convertNumber(input))
+    }
+
+    private fun convertNumber(input: String): Int {
         return runCatching {
             input.toInt()
         }.onFailure {
             throw IllegalArgumentException("구매금액을 숫자로 입력해주세요.")
         }.getOrThrow()
+    }
+
+    private fun validatePositive(input: Int): Int {
+        when (input > 0) {
+            true -> return input
+            false -> throw IllegalArgumentException("구매금액을 양수로 입력해주세요.")
+        }
     }
 }
--- src/test/kotlin/lotto/LottoTest.kt
+++ src/test/kotlin/lotto/LottoTest.kt
@@ -5,7 +5,8 @@ import io.kotest.core.spec.style.FreeSpec
 import io.kotest.inspectors.forAll
 import lotto.controller.LottoMachine
 
-private const val MONEY_ERROR_MESSAGE = "구매금액을 숫자로 입력해주세요."
+private const val NON_INTEGER_MESSAGE = "구매금액을 숫자로 입력해주세요."
+private const val NON_POSITIVE_MESSAGE = "구매금액을 양수로 입력해주세요."
 
 private val lottoMachine = LottoMachine()
 
@@ -13,7 +14,14 @@ class LottoTest : FreeSpec({
     "구매금액을 숫자로 입력하지 않으면 예외를 반환한다." {
         val testCases = listOf("bada", "바다", ">0<")
         testCases.forAll {
-            shouldThrowWithMessage<IllegalArgumentException>(MONEY_ERROR_MESSAGE) { lottoMachine.validateMoney(it) }
+            shouldThrowWithMessage<IllegalArgumentException>(NON_INTEGER_MESSAGE) { lottoMachine.validateMoney(it) }
+        }
+    }
+
+    "구매금액을 양수로 입력하지 않으면 예외를 반환한다." {
+        val testCases = listOf("-1000", "-291837918", "0")
+        testCases.forAll {
+            shouldThrowWithMessage<IllegalArgumentException>(NON_POSITIVE_MESSAGE) { lottoMachine.validateMoney(it) }
         }
     }
 })
