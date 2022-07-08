d63b038535dd032dfc68a9867bd8622869b0a50b
xrabcde <xrabcde@gmail.com>
2022-07-09 00:09:04 +0900

    refactor: 양수검증 require 사용하여 리팩터링

--- src/main/kotlin/lotto/domain/Money.kt
+++ src/main/kotlin/lotto/domain/Money.kt
@@ -4,15 +4,12 @@ class Money(input: String) {
     val price: Int
 
     init {
-        price = validateMoney(input)
+        price = convertNumber(input)
+        require(price > 0) { "구매금액을 양수로 입력해주세요." }
     }
 
     fun getLottoCount(): Int {
         return price / LOTTO_PRICE
-    }
-
-    private fun validateMoney(input: String): Int {
-        return validatePositive(convertNumber(input))
     }
 
     private fun convertNumber(input: String): Int {
@@ -21,13 +18,6 @@ class Money(input: String) {
         }.onFailure {
             throw IllegalArgumentException("구매금액을 숫자로 입력해주세요.")
         }.getOrThrow()
-    }
-
-    private fun validatePositive(input: Int): Int {
-        when (input > 0) {
-            true -> return input
-            false -> throw IllegalArgumentException("구매금액을 양수로 입력해주세요.")
-        }
     }
 
     companion object {
